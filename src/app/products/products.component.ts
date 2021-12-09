f3844b5a0f8e218fb5794b27ef8cdc4a3c986e60
Abraham Shimekt <shimektabraham@gmail.com>
2021-12-09 15:51:54 +0300

    component.ts is the logic of the page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './product.object';
 
 @Component({
   selector: 'app-products',
@@ -6,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  productName1="Nike"
-  productName2="Puma"
-  productName3="Sketcher"
+  // productName1="Nike"
+  // productName2="Puma"
+  // productName3="Sketcher"
+  product1:Product={
+    id: 1,
+    name: 'Nike',
+    price: 2000,
+    imageUrl: '../../assets/nike.jpg',
+    description: 'you can read more'    
+
+
+  }
+  product2:Product={
+    id: 2,
+    name: 'Puma',
+    price: 2000,
+    imageUrl: '../../assets/puma.jpg',
+    description: 'you can read more'    
+
+
+  }
+  product3:Product={
+    id: 3,
+    name: 'Sketcher',
+    price: 2000,
+    imageUrl: '../../assets/sketcher.jpg',
+    description: 'you can read more'    
+
+
+  }
 
   constructor() { }
 
